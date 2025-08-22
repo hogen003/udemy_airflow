aff44d63346bfee33386743772e648cdab5dc87f
Jacky <jg.v.hogen@mindefnl.onmicrosoft.com>
2025-08-22 14:04:08 +0200

    eerste commit

---
+++ dags/stock_market.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag
+from datetime import datetime
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',
+    catchup = False,
+    tags=['stock_market']
+)
+
+def stock_market();
+    pass    
